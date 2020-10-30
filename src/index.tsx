82d30c43e0f9623325c8942d7f8f30921f9f11d4
Giuseppe Setem <giuseppe7d@gmail.com>
2020-10-30 18:00:47 +0000

    feat: configure eslint, prettier and editorconfig

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Hello World</div>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<div>Hello World</div>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
