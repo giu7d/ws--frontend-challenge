d5f186ce70a7b0b9155ea84d4563519fd30edba4
Giuseppe Setem <giuseppe7d@gmail.com>
2020-11-02 16:02:58 +0000

    feat(tab): add animated transition

--- src/components/containers/Tabs/index.tsx
+++ src/components/containers/Tabs/index.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { ReactElement, useEffect, useState } from "react";
 
 import TabTitle from "../../fragments/TabTitle";
 import { Wrapper } from "./styles";
@@ -9,21 +10,43 @@ interface ITabsProps {
 
 export const Tabs: React.FC<ITabsProps> = ({ children }) => {
 	const [selectedTab, setSelectedTab] = useState(0);
+	const [isChangingTabs, setIsChangingTabs] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsChangingTabs(false);
+		}, 200);
+	}, [selectedTab]);
+
+	const handleSelect = (index: number) => {
+		setIsChangingTabs(true);
+		setSelectedTab(index);
+	};
 
 	return (
-		<Wrapper>
-			<nav>
-				{children.map((item, index) => (
-					<TabTitle
-						key={index}
-						title={item.props.title}
-						index={index}
-						isSelected={index === selectedTab}
-						onSelect={setSelectedTab}
-					/>
-				))}
-			</nav>
-			{children[selectedTab]}
-		</Wrapper>
+		<AnimatePresence exitBeforeEnter>
+			<Wrapper>
+				<nav>
+					{children.map((item, index) => (
+						<TabTitle
+							key={index}
+							title={item.props.title}
+							index={index}
+							isSelected={index === selectedTab}
+							onSelect={handleSelect}
+						/>
+					))}
+				</nav>
+				{!isChangingTabs && (
+					<motion.div
+						initial={{ opacity: 0, y: 100 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: 100 }}
+					>
+						{children[selectedTab]}
+					</motion.div>
+				)}
+			</Wrapper>
+		</AnimatePresence>
 	);
 };
